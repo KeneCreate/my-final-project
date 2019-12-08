3931340391c4224f5eab2e1467496005c9cafdd8
kene <kcmountain2002@yahoo.com>
2019-12-08 17:37:20 -0600

    additing pug

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static('public'))
+app.set('view engine','pug')
+
+app.get('',(req,res)=>{
+res.render('home',{})
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
